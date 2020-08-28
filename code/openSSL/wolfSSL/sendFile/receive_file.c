cae86c71b2369c032468c9f5c03f47234d4d1a06
Luca <luca97vale@gmail.com>
2020-08-28 11:48:35 +0200

    add wolfSSL sendFile program

---
+++ code/openSSL/wolfSSL/sendFile/receive_file.c
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include "transfer.h"
+/* wolfSSL */
+#include <wolfssl/options.h>
+#include <wolfssl/ssl.h>
+
+void writefile(WOLFSSL *ssl, FILE *fp);
+ssize_t total = 0;
+int main(int argc, char *argv[])
+{
+    WOLFSSL_CTX *ctx;
+    WOLFSSL *ssl;
+    /* Initialize wolfSSL */
+    wolfSSL_Init();
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1)
+    {
+        perror("Can't allocate sockfd");
+        exit(1);
+    }
+    if ((ctx = wolfSSL_CTX_new(wolfTLSv1_2_server_method())) == NULL)
+    {
+        fprintf(stderr, "ERROR: failed to create WOLFSSL_CTX\n");
+        return -1;
+    }
+
+    /* Load CA certificates into CYASSL_CTX */
+    if (wolfSSL_CTX_load_verify_locations(ctx, "./certs/ca-cert.pem", 0) != SSL_SUCCESS)
+    {
+        fprintf(stderr, "Error loading ./certs/ca-cert.pem, please check the file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Load server certificates into WOLFSSL_CTX */
+    if (wolfSSL_CTX_use_certificate_file(ctx, "./certs/server-cert.pem", SSL_FILETYPE_PEM) != SSL_SUCCESS)
+    {
+        fprintf(stderr, "Error loading ./certs/server-cert.pem, please check the file.\n");
+        exit(EXIT_FAILURE);
+    }
+    /* Load keys */
+    if (wolfSSL_CTX_use_PrivateKey_file(ctx, "./certs/server-key.pem", SSL_FILETYPE_PEM) != SSL_SUCCESS)
+    {
+        fprintf(stderr, "Error loading ./certs/server-key.pem, please check the file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in clientaddr, serveraddr;
+    memset(&serveraddr, 0, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons(SERVERPORT);
+
+    if (bind(sockfd, (const struct sockaddr *)&serveraddr, sizeof(serveraddr)) == -1)
+    {
+        perror("Bind Error");
+        exit(1);
+    }
+
+    if (listen(sockfd, LINSTENPORT) == -1)
+    {
+        perror("Listen Error");
+        exit(1);
+    }
+
+    socklen_t addrlen = sizeof(clientaddr);
+    int connfd = accept(sockfd, (struct sockaddr *)&clientaddr, &addrlen);
+    if (connfd == -1)
+    {
+        perror("Connect Error");
+        exit(1);
+    }
+    close(sockfd);
+    /* Create a WOLFSSL object */
+    if ((ssl = wolfSSL_new(ctx)) == NULL)
+    {
+        fprintf(stderr, "ERROR: failed to create WOLFSSL object\n");
+        return -1;
+    }
+    /* Attach wolfSSL to the socket */
+    wolfSSL_set_fd(ssl, connfd);
+    /* Establish TLS connection */
+    int ret = wolfSSL_accept(ssl);
+    if (ret != SSL_SUCCESS)
+    {
+        fprintf(stderr, "wolfSSL_accept error = %d\n", wolfSSL_get_error(ssl, ret));
+        return -1;
+    }
+
+    char filename[BUFFSIZE] = {0};
+    if (recv(connfd, filename, BUFFSIZE, 0) == -1)
+    {
+        perror("Can't receive filename");
+        exit(1);
+    }
+
+    FILE *fp = fopen("output.txt", "wb");
+    if (fp == NULL)
+    {
+        perror("Can't open file");
+        exit(1);
+    }
+
+    char addr[INET_ADDRSTRLEN];
+    printf("Start receive file: %s from %s\n", filename, inet_ntop(AF_INET, &clientaddr.sin_addr, addr, INET_ADDRSTRLEN));
+    writefile(ssl, fp);
+    printf("Receive Success, NumBytes = %ld\n", total);
+    wolfSSL_free(ssl);     /* Free the wolfSSL object              */
+    wolfSSL_CTX_free(ctx); /* Free the wolfSSL context object          */
+    wolfSSL_Cleanup();     /* Cleanup the wolfSSL environment          */
+    fclose(fp);
+    close(connfd);
+    return 0;
+}
+
+void writefile(WOLFSSL *ssl, FILE *fp)
+{
+    ssize_t n;
+    char buff[MAX_LINE] = {0};
+    clock_t t; 
+    t = clock(); 
+    while ((n = wolfSSL_read(ssl, buff, sizeof(buff))) > 0)
+    {
+        total += n;
+        if (n == -1)
+        {
+            perror("Receive File Error");
+            exit(1);
+        }
+        if (fwrite(buff, sizeof(char), n, fp) != n)
+        {
+            perror("Write File Error");
+            exit(1);
+        }
+        memset(buff, 0, MAX_LINE);
+    }
+    t = clock() - t; 
+    double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds 
+    printf("%f seconds to receive data \n", time_taken); 
+}
---
+++ code/openSSL/wolfSSL/sendFile/send_file.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libgen.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include "transfer.h"
+/* wolfSSL */
+#include <wolfssl/options.h>
+#include <wolfssl/ssl.h>
+
+void sendfile(FILE *fp, WOLFSSL *ssl);
+ssize_t total = 0;
+/* declare wolfSSL objects */
+WOLFSSL_CTX *ctx;
+int main(int argc, char *argv[])
+{
+    WOLFSSL *ssl;
+    if (argc != 3)
+    {
+        perror("usage:send_file filepath <IPaddress>");
+        exit(1);
+    }
+    /* Initialize wolfSSL */
+    wolfSSL_Init();
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        perror("Can't allocate sockfd");
+        exit(1);
+    }
+    /* Create and initialize WOLFSSL_CTX */
+    if ((ctx = wolfSSL_CTX_new(wolfTLSv1_2_client_method())) == NULL)
+    {
+        fprintf(stderr, "ERROR: failed to create WOLFSSL_CTX\n");
+        return -1;
+    }
+
+    /* Load CA certificates into WOLFSSL_CTX */
+    if (wolfSSL_CTX_load_verify_locations(ctx, "./certs/ca-cert.pem", 0) != SSL_SUCCESS)
+    {
+        fprintf(stderr, "Error loading ./certs/ca-cert.pem, please check the file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in serveraddr;
+    memset(&serveraddr, 0, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(SERVERPORT);
+    if (inet_pton(AF_INET, argv[2], &serveraddr.sin_addr) < 0)
+    {
+        perror("IPaddress Convert Error");
+        exit(1);
+    }
+
+    if (connect(sockfd, (const struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
+    {
+        perror("Connect Error");
+        exit(1);
+    }
+
+    /* Create a WOLFSSL object */
+    if ((ssl = wolfSSL_new(ctx)) == NULL)
+    {
+        fprintf(stderr, "ERROR: failed to create WOLFSSL object\n");
+        return -1;
+    }
+
+    /* Attach wolfSSL to the socket */
+    wolfSSL_set_fd(ssl, sockfd);
+    /* Connect to wolfSSL on the server side */
+    if (wolfSSL_connect(ssl) != SSL_SUCCESS)
+    {
+        fprintf(stderr, "ERROR: failed to connect to wolfSSL\n");
+        return -1;
+    }
+
+    char *filename = basename(argv[1]);
+    if (filename == NULL)
+    {
+        perror("Can't get filename");
+        exit(1);
+    }
+
+    char buff[BUFFSIZE] = {0};
+    strncpy(buff, filename, strlen(filename));
+    if (send(sockfd, buff, BUFFSIZE, 0) == -1)
+    {
+        perror("Can't send filename");
+        exit(1);
+    }
+
+    FILE *fp = fopen(argv[1], "rb");
+    if (fp == NULL)
+    {
+        perror("Can't open file");
+        exit(1);
+    }
+
+    sendfile(fp, ssl);
+    //puts("Send Success");
+    printf("Send Success, NumBytes = %ld\n", total);
+    fclose(fp);
+    /* Cleanup and return */
+    wolfSSL_free(ssl);     /* Free the wolfSSL object                  */
+    wolfSSL_CTX_free(ctx); /* Free the wolfSSL context object          */
+    wolfSSL_Cleanup();     /* Cleanup the wolfSSL environment          */
+    close(sockfd);         /* Close the connection to the server       */
+    return 0;
+}
+
+void sendfile(FILE *fp, WOLFSSL *ssl)
+{
+    int n;
+    char sendline[MAX_LINE] = {0};
+    while ((n = fread(sendline, sizeof(char), MAX_LINE, fp)) > 0)
+    {
+        total += n;
+        if (n != MAX_LINE && ferror(fp))
+        {
+            perror("Read File Error");
+            exit(1);
+        }
+        if (wolfSSL_write(ssl, sendline, strlen(sendline)) == -1)
+        {
+            perror("Can't send file");
+            exit(1);
+        }
+        memset(sendline, 0, MAX_LINE);
+    }
+}
